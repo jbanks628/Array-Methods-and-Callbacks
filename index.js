3ae0412945cb9055ac762de1a2cf15d1d968c5c0
Jada Banks <jadabanks@Jadas-MacBook-Air.local>
2021-10-12 18:56:15 -0500

    Almost Done

--- index.js
+++ index.js
@@ -4,30 +4,38 @@ const { fifaData } = require('./fifa.js')
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
-
+const finals2014 = fifaData.filter(function(item){
+    return item['Year'] === 2014 && item['Stage'] === 'Final';
+});
+console.log(finals2014);
 //(a) Home Team name for 2014 world cup final
-
+console.log('Task 1a', finals2014[0]['Home Team Name']);
 //(b) Away Team name for 2014 world cup final
-
+console.log('Task 1b', finals2014[0]['Away Team Name']);
 //(c) Home Team goals for 2014 world cup final
-
+console.log('Task 1c', finals2014[0]['Home Team Goals']);
 //(d) Away Team goals for 2014 world cup final
-
+console.log('Task 1d', finals2014[0]['Away Team Goals']);
 //(e) Winner of 2014 world cup final */
-
+console.log('Task 1e', finals2014[0]['Win conditions']);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use getFinals to do the following:
 1. Receive data as a parameter
 2. Return an array of objects with the data of the teams that made it to the final stage
-
+//use .filter
 hint - you should be looking at the stage key inside of the objects
 */
 
-function getFinals(/* code here */) {
-   /* code here */
+function getFinals(data) {
+
+  return data.filter(function(item) {
+     return item['Stage'] === 'Final';
+     
+  });
 }
 
+console.log('Task 2', getFinals(fifaData));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -36,10 +44,14 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-    /* code here */
-}
-
+function getYears(array, getFinalsCb) {
+    const years = [];
+    for(let i = 0; i < getFinalsCb(array).length; i++){
+    years.push(getFinalsCb(array)[i]['Year']);
+};
+return years;
+};
+console.log(getYears(fifaData, getFinals));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -48,10 +60,25 @@ Use the higher-order function getWinners to do the following:
 2. Receives the callback function getFinals from task 2 
 3. Determines the winner (home or away) of each `finals` game. 
 4. Returns the names of all winning countries in an array called `winners` */ 
+//compare the points/goals to determine winner; don't care about overtime
+function getWinners(array, getFinalsCb) {
+   
+    const winners = getFinalsCb(array).map(function(item){
+        if(item['Home Team Goals'] > item['Away Team Goals']){
+            return item['Home Team Name']; 
+        }
+        else //if (item['Away Team Goals'] > item['Home Team Goals'])
+        {
+            return item['Away Team Name'];
+        };
+      
+    })
+    return winners;
 
-function getWinners(/* code here */) {
-    /* code here */
 }
+
+
+console.log(getWinners(fifaData, getFinals));
 
 
 
@@ -64,11 +91,17 @@ Use the higher-order function getWinnersByYear to do the following:
 
 hint: the strings returned need to exactly match the string in step 4.
  */
-
-function getWinnersByYear(/* code here */) {
-    /* code here */
+//use index in addition to item to grab index of one array & item of other array
+function getWinnersByYear(array, getYearsCb, getWinnersCb) {
+    getYearsCb = getYears(array, getFinals);
+    getWinnersCb = getWinners(array, getFinals);
+    getWinnersCb(array, getFinals).map(function(item[i]){
+        return item[i](getYearsCb);
+    }
+    )
+    return `In ${getYearsCb}, ${getWinnersCb} won the world cup!`;
 }
-
+getWinnersByYear(fifaData, getYears, getWinners);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -77,7 +110,7 @@ Use the higher order function getAverageGoals to do the following:
  2. Return the the average number of the total home team goals and away team goals scored per match and round to the second decimal place. 
  
  (Hint: use .reduce and do this in 2 steps) 
- 
+ //if want to round to 2nd decimal, use .toFixed(2);
  Example of invocation: getAverageGoals(getFinals(fifaData));
 */
 
